7959370edb7ba259e30445e58eaf7c75a85001fe
David <davidchilds6@gmail.com>
2021-04-28 09:12:38 +0100

    working transform

--- files_and_error_handling/reading_transforming_writing_templates.py
+++ files_and_error_handling/reading_transforming_writing_templates.py
@@ -6,7 +6,7 @@ def extract_csv(csv_name):
     try:
         with open(csv_name) as csv_file:
             readfile = csv.reader(csv_file, delimiter=",")
-            csv_list = iter(list(readfile))
+            csv_list = list(readfile)
         return csv_list
     except FileNotFoundError:
         print("File not found")
@@ -19,17 +19,18 @@ def extract_csv(csv_name):
 def transform_csv(csv_list):
     filtered_csv = []
     for entry in csv_list:
-        filtered_csv.append([entry[1], entry[2], entry[4]])  # Select columns to filter
+        columns = entry[1], entry[2], entry[4]  # Select columns to filter
+        filtered_csv.append(columns)
     return filtered_csv
 
 
 def load_csv(old_csv, new_csv):
-    new_user_details = transform_csv(old_csv)
+    update = transform_csv(extract_csv(old_csv))
     with open(new_csv, "w") as new_file:
         csv_writer = csv.writer(new_file)
-        csv_writer.writerows(new_user_details)
+        csv_writer.writerows(update)
 
 
-e_ud = extract_csv("user_details.csv")
-t_ud = transform_csv(e_ud)
-load_csv(e_ud, t_ud)
+# e_ud = extract_csv("user_details.csv")
+# t_ud = transform_csv(e_ud)
+load_csv("user_details.csv", new_csv="hope_this_works")
