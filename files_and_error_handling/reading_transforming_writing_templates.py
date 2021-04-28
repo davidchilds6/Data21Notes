b3bee1c8bcbb78b6cfc65b998695ad1c8180583a
David <davidchilds6@gmail.com>
2021-04-28 09:46:03 +0100

    working generic functions for filtering columns

--- files_and_error_handling/reading_transforming_writing_templates.py
+++ files_and_error_handling/reading_transforming_writing_templates.py
@@ -18,19 +18,31 @@ def extract_csv(csv_name):
 
 def transform_csv(csv_list):
     filtered_csv = []
-    for entry in csv_list:
-        columns = entry[1], entry[2], entry[4]  # Select columns to filter
-        filtered_csv.append(columns)
-    return filtered_csv
+    try:
+        for entry in csv_list:
+            columns = entry[1], entry[2], entry[4]  # Select columns to filter
+            filtered_csv.append(columns)
+        return filtered_csv
+    except FileNotFoundError:
+        print("File not found")
+    except FileExistsError:
+        print("The file does not exist")
+    finally:
+        print("File transformed")
 
 
 def load_csv(old_csv, new_csv):
-    update = transform_csv(extract_csv(old_csv))
-    with open(new_csv, "w") as new_file:
-        csv_writer = csv.writer(new_file)
-        csv_writer.writerows(update)
+    transformed_file = transform_csv(extract_csv(old_csv))
+    try:
+        with open(new_csv, "w") as new_file:
+            csv_writer = csv.writer(new_file)
+            csv_writer.writerows(transformed_file)
+    except FileNotFoundError:
+        print("File not found")
+    except FileExistsError:
+        print("The file does not exist")
+    finally:
+        print("Transformed file loaded")
 
 
-# e_ud = extract_csv("user_details.csv")
-# t_ud = transform_csv(e_ud)
 load_csv("user_details.csv", new_csv="hope_this_works.csv")
