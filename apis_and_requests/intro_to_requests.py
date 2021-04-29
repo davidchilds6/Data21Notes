44a55274cf326de584468a9f7c857cf4f36e15e8
David <davidchilds6@gmail.com>
2021-04-29 11:51:12 +0100

    update

--- apis_and_requests/intro_to_requests.py
+++ apis_and_requests/intro_to_requests.py
@@ -2,19 +2,19 @@ import requests
 import json
 from pprint import pprint
 
-# # simple request
-# post_code_req = requests.get("https://api.postcodes.io/postcodes/dt94ls")
-#
-# print(post_code_req)
-# print(post_code_req.headers)
-# print(post_code_req.content)
-# print(type(post_code_req.content))
-# print(type(post_code_req.json))
-# print(post_code_req.json())   # Returns it in a nice json string
+# simple request
+post_code_req = requests.get("https://api.postcodes.io/postcodes/dt94ls")
 
-# # Multiple postcode look up
+print(post_code_req)
+print(post_code_req.headers)
+print(post_code_req.content)
+print(type(post_code_req.content))
+print(type(post_code_req.json))
+pprint(post_code_req.json())   # Returns it in a nice json string
 
-json_body = json.dumps({"postcodes": ["PR3 0SG", "M45 6GN", "EX165BL"]})
+# Multiple postcode look up
+
+json_body = json.dumps({"postcodes": ["PR3 0SG"]})
 headers = {"Content-Type": "application/json"}
 
 postcode_multi_req = requests.post("https://api.postcodes.io/postcodes", headers=headers, data=json_body)
