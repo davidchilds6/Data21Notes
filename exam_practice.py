86d80673c969d68c206cbc77a6a5fa9ec5e7ff6b
David <davidchilds6@gmail.com>
2021-04-29 08:58:01 +0100

    Stuck on fibonacci

--- exam_practice.py
+++ exam_practice.py
@@ -111,4 +111,7 @@ Note : The Fibonacci Sequence is the series of numbers :
 Every next number is found by adding up the two numbers before it.
 Expected Output : 1 1 2 3 5 8 13 21 34"""
 
-numbers
+"""CLASSES PRACTICE"""
+
+"""https://campus.datacamp.com/courses/object-oriented-programming-in-python/oop-fundamentals?ex=5"""
+
