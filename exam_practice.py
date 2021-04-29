56af5b7a400e277712f146e9be505e79b6021e7c
David <davidchilds6@gmail.com>
2021-04-29 20:57:32 +0100

    more practice

--- exam_practice.py
+++ exam_practice.py
@@ -111,7 +111,33 @@ Note : The Fibonacci Sequence is the series of numbers :
 Every next number is found by adding up the two numbers before it.
 Expected Output : 1 1 2 3 5 8 13 21 34"""
 
+# x = 0
+# while x + 1 < 50:
+#     print(x+1)
+#     x, (x + 1) = x + 1, x + x + 1
+#
+
+# x, y = 0, 1
+# while y < 50:
+#     print(y)
+#     x, y = y, x + y
+
 """CLASSES PRACTICE"""
 
 """https://campus.datacamp.com/courses/object-oriented-programming-in-python/oop-fundamentals?ex=5"""
+
+"""Write the code for a Python function expand(x) that takes a list of strings,
+concatenates them, and returns the resulting string repeated three times."""
+
+
+# def expand(x):  # not very good solution, doesn't actually return a string
+#     for i in range(3):
+#         for j in x:
+#             print(j, end="")
+#
+#
+# def expand2(x):
+#     return ''.join(x) * 3
+
+
 
