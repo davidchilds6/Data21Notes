4a3044579e17de0649cc617b3c17eac753032d7b
David <davidchilds6@gmail.com>
2021-05-24 16:54:47 +0100

    having a play

--- apis_and_requests/postcode_package_copy.py
+++ apis_and_requests/postcode_package_copy.py
@@ -8,18 +8,24 @@ class PostCode:
         self.postcode = post_code
         self.column_list = columns
 
-    def retrieve_postcode_details(self):
+    def retrieve_all_details(self):
         postcode_details = requests.get(f"https://api.postcodes.io/postcodes/{self.postcode}")
         return dict(postcode_details.json())["result"].items()
 
-    def select_values(self):
-        postcode_details_dictionary = self.retrieve_postcode_details()
+    def retrieve_selected_details(self):
+        postcode_details_dictionary = self.retrieve_all_details()
         return [(key, value) for (key, value) in postcode_details_dictionary if key in self.column_list]
 
 
-def get_columns():
-    postcode_details = requests.get(f"https://api.postcodes.io/postcodes/HP22 6DB")
+def get_columns():  # Call this function to just get a list of potential fields
+    postcode_details = requests.get(f"https://api.postcodes.io/random/postcodes")
     postcode_details_dictionary = dict(postcode_details.json())["result"].items()
     key_list = [key for (key, value) in postcode_details_dictionary]
     return key_list
 
+
+my_code = PostCode("dt94ls", ["postcode", "parliamentary_constituency", "admin_county", "longitude", "latitude"])
+pprint(my_code.retrieve_all_details(), sort_dicts=False)
+pprint(my_code.retrieve_selected_details(), sort_dicts=False)
+
+# pprint(get_columns(), sort_dicts=False)
